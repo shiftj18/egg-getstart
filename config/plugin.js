69e52b4ebe98c5c84e23a17fba1d51d897850508
阿劭 <tianjie.stj@alibaba-inc.com>
2020-05-27 10:49:41 +0800

    /news

---
+++ config/plugin.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// exports.nunjucks = {
+//   enable: true,
+//   package: 'egg-view-nunjucks'
+// }
+
+/** @type Egg.EggPlugin */
+module.exports = {
+  // had enabled by egg
+  // static: {
+  //   enable: true,
+  // }
+  nunjucks: {
+    enable: true,
+    package: 'egg-view-nunjucks'
+  }
+};
